add388173ff278048192efd8e6bbc8c396c9f055
claude17 <claude.mta@gmail.com>
2026-05-22 04:44:39 +0600

    added issues route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import express, { type Application, type Request, type Response } from "express";
 import { authRoute } from "./modules/auth/auth.route";
+import { issuesRoute } from "./modules/issues/issues.route";
 const app: Application = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -10,5 +19,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/auth", authRoute);
+
+app.use("/api/issues", issuesRoute);
 
 export default app;
