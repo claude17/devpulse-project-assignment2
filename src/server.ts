734b8e7e2e465f02025e115e66bd4dac66f1c031
claude17 <claude.mta@gmail.com>
2026-05-23 09:29:11 +0600

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import app from "./app";
+import { initDB } from "./db";
 const port = 7000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+initDB();
 const main = () => {
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
