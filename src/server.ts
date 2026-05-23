862cc0533b6ffb18dc67939566c7dd47ff65efd9
claude17 <claude.mta@gmail.com>
2026-05-23 09:32:31 +0600

    removed comment

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,6 @@ import app from "./app";
 import { initDB } from "./db";
 const port = 7000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 initDB();
 const main = () => {
   app.listen(port, () => {
