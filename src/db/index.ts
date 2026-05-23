5dbfdc8fa8221d760579a9e8248da71e88e109fa
claude17 <claude.mta@gmail.com>
2026-05-23 07:43:07 +0600

    updated both tables with checks

--- src/db/index.ts
+++ src/db/index.ts
@@ -13,7 +13,21 @@ export const initDB = async () => {
       name VARCHAR(20),
       email VARCHAR(30) UNIQUE NOT NULL,
       password TEXT NOT NULL,
-      role VARCHAR(20) NOT NULL DEFAULT 'contributor',
+      role VARCHAR(20) NOT NULL DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS issues(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(150) NOT NULL,
+      description TEXT NOT NULL,
+      type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+      status VARCHAR(20) NOT NULL DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+      reporter_id INT NOT NULL,
 
       created_at TIMESTAMP DEFAULT NOW(),
       updated_at TIMESTAMP DEFAULT NOW()
